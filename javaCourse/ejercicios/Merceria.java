3c545b25682a1332f15bca8ccf7d923df8e3abc7
Patricio Avila <152195893+Avila-Pato@users.noreply.github.com>
2025-07-10 23:57:21 -0400

    Refactor Merceria class to improve input handling and add discount logic based on total amount

--- javaCourse/ejercicios/Merceria.java
+++ javaCourse/ejercicios/Merceria.java
@@ -3,29 +3,46 @@ import java.util.Scanner;
 public class Merceria {
 
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
+        
         int cantidadPaquetes;
-        double montoTotal;
+        double montoTotal, diferencia, descuento, totalConDescuento;
+        Scanner teclado = new Scanner(System.in);
 
         System.out.println("Ingrese la cantidad de paquetes:");
         cantidadPaquetes = teclado.nextInt();
-
-        if (cantidadPaquetes < 5) {
-            System.out.println("No se permiten ventas menores a 5");
-        } else if (cantidadPaquetes >= 5 && cantidadPaquetes <= 15) {
-            System.out.println("El costo de envío es de 10 USD");
-        } else {
-            System.out.println("Usted no tiene costo de envío. Muchas gracias por su compra");
-        }
-
-        System.out.println("Ingrese el monto total de la compra:");
-        montoTotal = teclado.nextDouble();
-
         teclado.close();
 
-        System.out.println("Resumen:");
-        System.out.println("Cantidad de paquetes: " + cantidadPaquetes);
-        System.out.println("Monto total: $" + montoTotal);
+        // posibles casos
+
+        if (cantidadPaquetes < 5) {
+            if (cantidadPaquetes < 0) {
+                System.out.println("La cantidad de paquetes no puede ser negativa");
+            } else {
+                System.out.println("No se permiten ventas minoristas (cantidad de paqutes menor a 5)");
+            }
+        } else {
+            System.out.println("Ingrese el monto total de la compra");
+            teclado = new Scanner(System.in);
+            montoTotal = teclado.nextDouble();
+
+            if (montoTotal < 100) {
+                diferencia = 100 - montoTotal;
+                System.out.println("El monto es menor a 100 por lo que no se posee las promociones. Nececita comprar" + diferencia + "USD para entrar en la promo");
+            } else {
+                if (montoTotal >= 100 && montoTotal <= 300) {
+                    descuento = montoTotal * 0.05;
+                    totalConDescuento = montoTotal - descuento;
+                    System.out.println("Por su compra tiene un descuento del 5% que equivale a: " + descuento
+                            + " El monto total con descuento es de: " + totalConDescuento);
+                } else {
+                    descuento = montoTotal * 0.10;
+                    totalConDescuento = montoTotal - descuento;
+                     System.out.println("Por su compra tiene un descuento del 10% que equivale a: " + descuento
+                            + " El monto total con descuento es de: " + totalConDescuento);
+                }
+            }
+        }
+
+        
     }
 }
